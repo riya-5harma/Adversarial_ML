e5f841f97b60e404363b6369d82d0b84128bc8e4
riya-5harma <work.riya.sharma@gmail.com>
2023-06-06 11:20:07 +0530

    changes done

---
+++ Model/model.py
@@ -0,0 +1,32 @@
+import numpy 
+from tensorflow.keras.datasets import mnist
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# Normalize pixel values to the range [0, 1]
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Reshape the data to match the input shape expected by ART (batch size, width, height, channels)
+x_train = numpy.reshape(x_train, (x_train.shape[0], 28, 28, 1))
+x_test = numpy.reshape(x_test, (x_test.shape[0], 28, 28, 1))
+
+# Convert labels to one-hot encoding
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(loss=tf.keras.losses.categorical_crossentropy,
+              optimizer=tf.keras.optimizers.legacy.Adam(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=1)
