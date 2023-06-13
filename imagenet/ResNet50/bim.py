31f84de527fef4c8016482a876a19821fc2440a4
Chirag Shangle <shanglechiru3@gmail.com>
2023-06-13 19:35:57 +0530

    updated

--- imagenet/ResNet50/bim.py
+++ imagenet/ResNet50/bim.py
@@ -24,24 +24,6 @@ x = preprocess_input(x)
 # Get the true label of the image
 true_label = np.argmax(classifier.predict(x), axis=1)
 
-# Configure the BIM attack
-# attack = BasicIterativeMethod(estimator=classifier, eps=0.0, eps_step=0.1, max_iter=10)
-
-# Generate adversarial examples
-# x_adv = attack.generate(x, y=np.argmax(to_categorical(true_label, 1000), axis=1))
-
-# Evaluate the attack
-# preds_original = classifier.predict(x)
-# preds_adv = classifier.predict(x_adv)
-
-# print('Shape of preds_original:', preds_original.shape)
-# print('Shape of preds_adv:', preds_adv.shape)
-
-# label_original = decode_predictions(preds_original, top=1)[0][0][1]
-# label_adv = decode_predictions(preds_adv, top=1)[0][0][1]
-
-# print('Original Image Prediction:', label_original)
-# print('Adversarial Image Prediction:', label_adv)
 
 for epss in epsilons:
     
--- imagenet/ResNet50/deepfool.py
+++ imagenet/ResNet50/deepfool.py
@@ -8,6 +8,7 @@ tf.compat.v1.disable_eager_execution()
 # Load the ResNet50 model
 model = ResNet50(weights='imagenet')
 
+epsilons = [0, .05, .1, .15, .2, .25, .3]
 # Load and preprocess a sample image
 image_path = get_file('sample_image.jpg', 'https://cdn.britannica.com/70/192570-138-848FB7B3/penguin-species-places-Galapagos-Antarctica.jpg?w=800&h=450&c=crop')
 image = tf.keras.preprocessing.image.load_img(image_path, target_size=(224, 224))
@@ -18,24 +19,20 @@ x = preprocess_input(x)
 # Create an ART classifier
 classifier = KerasClassifier(model=model, clip_values=(0, 255))
 
-# Initialize the Deep Fool attack
-attack = DeepFool(classifier)
 
-# Generate adversarial examples
-x_adv = attack.generate(x)
+for epss in epsilons:
+    attack = DeepFool(classifier,epsilon=epss)
+    adversarial_image = attack.generate(x)  
+    preds_original = classifier.predict(x)
+    preds_adv = classifier.predict(adversarial_image)
+    original_label = decode_predictions(preds_original, top=3)[0]
+    adversarial_label = decode_predictions(preds_adv, top=3)[0]
+    
+    print("For epsilon : ",epss)
+    print("Original Image Predictions:")
+    for class_name, class_description, class_probability in original_label:
+        print(f"- {class_description}: {class_probability:.2%}")
 
-# Make predictions on the original and adversarial examples
-predictions = model.predict(x)
-adv_predictions = model.predict(x_adv)
-
-# Decode and print the predictions
-original_label = decode_predictions(predictions, txop=3)[0]
-adversarial_label = decode_predictions(adv_predictions, top=3)[0]
-
-print("Original Image Predictions:")
-for class_name, class_description, class_probability in original_label:
-    print(f"- {class_description}: {class_probability:.2%}")
-
-print("\nAdversarial Image Predictions:")
-for class_name, class_description, class_probability in adversarial_label:
-    print(f"- {class_description}: {class_probability:.2%}")
+    print("\nAdversarial Image Predictions:")
+    for class_name, class_description, class_probability in adversarial_label:
+        print(f"- {class_description}: {class_probability:.2%}")
